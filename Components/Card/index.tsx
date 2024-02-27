f383ff8c31028c340cafbe7630dd4533e714479d
Mizerski <mizerskimachado@hotmail.com>
2024-02-27 14:13:22 -0300

    init

---
+++ Components/Card/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View , Text} from "react-native";
+
+
+
+export function Card() {
+    return (
+      <View>
+        <Text>Card</Text>
+      </View>
+    );
+}
---
+++ Styles/Colors.ts
@@ -0,0 +1,8 @@
+export const colors={
+
+    soft_green:'#9CCC65',
+    soft_pink: '#F48FB1',
+    light_blue: '#81D4FA',
+    light_gray: '#E0E0E0',
+    cream: '#FFE0B2',
+}
